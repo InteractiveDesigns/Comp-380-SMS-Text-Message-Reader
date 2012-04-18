ce704768695be1976ed7b73f2913f7a76748fe9f
Juan Flores <carlos_k101@yahoo.com>
2012-04-18 16:51:15 -0700

    i'm committing my class SMSTextMessageInfo.java

---
+++ src/comp380/Project/SMSTextReceiver/SMSTextMessageInfo.java
@@ -0,0 +1,26 @@
+package comp380.Project.SMSTextReceiver;
+
+public class SMSTextMessageInfo 
+{
+
+	public String m_Adress;
+	public String m_Message;
+	
+	public String getAdress()
+	{
+		return m_Adress;
+		
+	}
+	
+	public String getMessage()
+	{
+		return m_Message;
+	}
+	
+	
+	public  SMSTextMessageInfo(String newAdress, String newMessage)
+	{
+		
+	}
+
+}
