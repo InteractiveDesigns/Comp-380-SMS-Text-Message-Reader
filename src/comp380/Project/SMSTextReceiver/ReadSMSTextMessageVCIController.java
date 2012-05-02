993cc6714f739a2e44c9218a4933435b44fd7ab8
matt <mttmsgrv@yahoo.com>
2012-05-02 15:47:07 -0700

    Added code to allow us to implement voice recognition

--- src/comp380/Project/SMSTextReceiver/ReadSMSTextMessageVCIController.java
+++ src/comp380/Project/SMSTextReceiver/ReadSMSTextMessageVCIController.java
@@ -1,17 +1,22 @@
 package comp380.Project.SMSTextReceiver;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
 
-public class ReadSMSTextMessageVCIController extends UIController implements OnInitListener
+public class ReadSMSTextMessageVCIController extends UIController implements OnInitListener, RecognitionListener
 {
 	private TextToSpeech mTts;
 	private UserInterface ui;
 	private static final int REQUEST_CODE = 15850;		
-    
+
+	private SpeechRecognizer m_SpeechRecognizer;
+	
 	/**
 	 * Creates a new instance of ReadSMSTextMessageVCIController given the parent user interface object
 	 * 
@@ -21,6 +26,7 @@ public class ReadSMSTextMessageVCIController extends UIController implements OnI
 	{
 		super(userInterface);
 		ui = userInterface;
+		m_SpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(userInterface.getMainActivity().getBaseContext());
 	}
 	
 	/**
@@ -30,6 +36,7 @@ public class ReadSMSTextMessageVCIController extends UIController implements OnI
 	public void initializeUI()
 	{
 		// TODO Auto-generated method stub
+		m_SpeechRecognizer.setRecognitionListener(this);
 	}
 
 	/**
@@ -84,4 +91,51 @@ public class ReadSMSTextMessageVCIController extends UIController implements OnI
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak");
         ui.getMainActivity().startActivityForResult(intent, REQUEST_CODE);
     }
+
+	public void onBeginningOfSpeech() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onBufferReceived(byte[] arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onEndOfSpeech() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onError(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onEvent(int arg0, Bundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onPartialResults(Bundle arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onReadyForSpeech(Bundle arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onResults(Bundle arg0)
+	{
+		arg0.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+		// TODO: match the word just read to ignore, read, or replay and create a user command based on what was matched and call userRequestReceived
+		//m_UserInterface.userRequestReceived();
+	}
+
+	public void onRmsChanged(float arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
