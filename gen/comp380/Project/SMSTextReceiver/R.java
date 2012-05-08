b1aaf3a5e128c2e052d3f63c629f61795fb18cd2
juan <carlos_k101@yahoo.com>
2012-05-07 21:43:26 -0700

    ChNGES TO gui FOR VERSION 2

--- gen/comp380/Project/SMSTextReceiver/R.java
+++ gen/comp380/Project/SMSTextReceiver/R.java
@@ -14,10 +14,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int readLater=0x7f050003;
-        public static final int readNow=0x7f050000;
-        public static final int textView1=0x7f050001;
-        public static final int textView2=0x7f050002;
+        public static final int textView1=0x7f050000;
     }
     public static final class layout {
         public static final int text_view=0x7f030000;
